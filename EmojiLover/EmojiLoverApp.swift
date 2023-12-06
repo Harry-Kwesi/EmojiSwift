bc064b777d6ca1a37bacfce6652d6e8efa050ffb
Harry-Kwesi <harrydegraft@hotmail.com>
2023-12-06 18:52:26 +0530

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Harry Kwesi De Graft on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
