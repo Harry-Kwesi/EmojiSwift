7b2fef99e8813c933f810590062bf53b99d8b935
Harry-Kwesi <harrydegraft@hotmail.com>
2023-12-21 23:05:25 +0530

    final product

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -2,20 +2,33 @@
 //  ContentView.swift
 //  EmojiLover
 //
-//  Created by Harry Kwesi De Graft on 06/12/23.
+//  Created by Harry Kwesi De Graft on 05/12/23.
 //
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable {
+    case 😌, 😀, 😂, 😎, 🤪
+}
 struct ContentView: View {
+    @State var selection: Emoji = .😀
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self){
+                        emoji in Text(emoji.rawValue)
+                    }
+                }
+                .navigationTitle("The Emoji life!")
+                .pickerStyle(.segmented)
+            }
+            .padding()
         }
-        .padding()
+
+
     }
 }
 
---
+++ EmojiLover/EmojiLoverApp~.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Harry Kwesi De Graft on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
